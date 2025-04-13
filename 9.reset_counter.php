eff07be9ed2d1ff6f54fcbcc6b4c68420d0352f2
angus200493 <angus200493@gmail.com>
2025-04-13 12:46:49 +0800

    Update 9.reset_counter.php

--- 9.reset_counter.php
+++ 9.reset_counter.php
@@ -1,7 +1,7 @@
 <?php
-    session_start();
-    unset($_SESSION["counter"]);
-    echo "counter重置成功....";
-    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";
+    session_start();#用於開始或恢復現有會話
+    unset($_SESSION["counter"]);#清除計數器刪除會話中的counter變數
+    echo "counter重置成功....";#顯示counter重製成功
+    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";#兩秒後跳轉到8.counter.php
 
 ?>
