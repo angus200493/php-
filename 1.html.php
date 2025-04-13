f99778b32ea83ee9b0419de16bdab8ca4f13cb40
angus200493 <angus200493@gmail.com>
2025-04-13 11:26:22 +0800

    Update 1.html.php

--- 1.html.php
+++ 1.html.php
@@ -1,8 +1,8 @@
 <html>
     <head><title>html+php</title></head>      
     <body>
-        <?php echo "Hello world!"; ?> 
-        <h1>html</h1>
+        <?php echo "Hello world!"; ?> #echo用於輸出文字Hello world
+        <h1>html</h1>                 #結合HTML的<h1>標題
         <?php echo "Hello world!"; ?> 
         <h1>html</h1>
         <?php echo "Hello world!"; ?> 
