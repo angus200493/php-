815571745803e48e03d77fc17612e30f9b183689
angus200493 <angus200493@gmail.com>
2025-04-13 12:02:51 +0800

    Update 12.logout.php

--- 12.logout.php
+++ 12.logout.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    unset($_SESSION["id"]);
+    unset($_SESSION["id"]);#刪除存在在會話中的的變數
     echo "登出成功....";
-    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";
+    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";#等待3秒後跳無轉到2.login.html
 
 ?>
