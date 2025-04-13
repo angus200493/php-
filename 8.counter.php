cb96fb5d33cfcd2e2851d772c314f83cf4fbda6a
angus200493 <angus200493@gmail.com>
2025-04-13 12:44:04 +0800

    Update 8.counter.php

--- 8.counter.php
+++ 8.counter.php
@@ -1,10 +1,10 @@
 <?php
-    session_start();
-    if (!isset($_SESSION["counter"]))
-        $_SESSION["counter"]=1;
+    session_start();#用於開始或恢復現有會話
+    if (!isset($_SESSION["counter"]))#檢查$_SESSION["counter"]是否存在
+        $_SESSION["counter"]=1;#初始化$_SESSION["counter"]為1
     else
-        $_SESSION["counter"]++;
+        $_SESSION["counter"]++;#若$_SESSION["counter"]存在將其+1
 
-    echo "counter=".$_SESSION["counter"];
-    echo "<br><a href=9.reset_counter.php>重置counter</a>";
+    echo "counter=".$_SESSION["counter"];#顯示$_SESSION["counter"]當前數字
+    echo "<br><a href=9.reset_counter.php>重置counter</a>";#連接到到 9.reset_counter.php ，這個會用來重置計數器
 ?>
