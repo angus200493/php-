9724b29bd2fd6036791406518a65ec335b83fbaa
angus200493 <angus200493@gmail.com>
2025-04-13 11:38:48 +0800

    Update 10.login.php

--- 10.login.php
+++ 10.login.php
@@ -8,17 +8,17 @@
    while ($row=mysqli_fetch_array($result)) {
      if (($_POST["id"]==$row["id"]) && ($_POST["pwd"]==$row["pwd"])) {
        $login=TRUE;
-     }
+     }#檢查資料庫的帳號密碼與交出的資料是否一樣
    } 
    if ($login==TRUE) {
     session_start();
-    $_SESSION["id"]=$_POST["id"];
+    $_SESSION["id"]=$_POST["id"];# 儲存使用者帳號到會話變數 $_SESSION["id"] 將帳號儲存，以便後續網頁使用
     echo "登入成功";
     echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
-   }
+   }# 使用meta http-equiv=REFRESH ，3 秒後跳轉到 11.bulletin.php
 
   else{
-    echo "帳號/密碼 錯誤";
+    echo "帳號/密碼 錯誤";#顯示錯誤
     echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
-  }
+  }#使用 meta http-equiv=REFRESH，3 秒後跳轉到 2.login.html
 ?>
