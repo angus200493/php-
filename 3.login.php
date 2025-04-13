4770a263b465efdcc1b18b97a2c12369b4dcd2a4
angus200493 <angus200493@gmail.com>
2025-04-13 12:16:20 +0800

    Update 3.login.php

--- 3.login.php
+++ 3.login.php
@@ -1,6 +1,6 @@
 <?php 
     if (($_POST["id"] == "john") && ($_POST["pwd"]=="john1234"))
-        echo "登入成功";
+        echo "登入成功";#帳號john 密碼 john1234 是這樣則顯示登入成功
     else
-        echo "登入失敗";
+        echo "登入失敗";#帳號密碼不是則顯示登入失敗
 ?>
