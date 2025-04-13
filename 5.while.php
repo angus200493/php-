b5e2ada5fde224b7dd4908b170add38e62426bc4
angus200493 <angus200493@gmail.com>
2025-04-13 12:25:40 +0800

    Update 5.while.php

--- 5.while.php
+++ 5.while.php
@@ -4,7 +4,7 @@
    #mysqli_query() 從資料庫查詢資料
    $result=mysqli_query($conn, "select * from user");
    #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
-   while ($row=mysqli_fetch_array($result)) {
-     echo $row["id"]." ".$row["pwd"]."<br>";
+   while ($row=mysqli_fetch_array($result)) {#mysqli_fetch_array() 每次提取一行資料，儲存為陣列 $row
+     echo $row["id"]." ".$row["pwd"]."<br>";#顯示$row["id"] 和 $row["pwd"]分別是資料庫中的id 和 pwd 欄位值
    } 
 ?>
